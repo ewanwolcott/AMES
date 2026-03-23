e57eab71726991310be34f0959275ca54ce272e7
MUKILTEO\1725608 <Talonlee44@gmail.com>
2026-03-23 08:53:28 -0700

    Scorpion no longer blurry

--- Assets/TalonScripts/PlayerUpgrades.cs
+++ Assets/TalonScripts/PlayerUpgrades.cs
@@ -8,11 +8,13 @@ public class PlayerUpgrades : MonoBehaviour
 
     PlayerCombat playerCombat;
     PlayerMovement playerMovement;
+    PlayerHealth playerHealth;
 
     private void Awake()
     {
         playerCombat = GetComponent<PlayerCombat>();
         playerMovement = GetComponent<PlayerMovement>();
+        playerHealth = GetComponent<PlayerHealth>();
     }
 
     private void Update()
@@ -35,5 +37,13 @@ public class PlayerUpgrades : MonoBehaviour
             playerMovement.movementSpeed = 6f;
             playerMovement.jumpHeight = 11f;
         }
+        if (healthLevel == 2)
+            {
+                playerHealth.maxHealth = 6;
+            }
+        else if(healthLevel == 3)
+            {
+                playerHealth.maxHealth = 7;
+            }
     }
 }
