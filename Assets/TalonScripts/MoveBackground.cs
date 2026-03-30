fe64b35f19a40ef6aa1e34feeb1893e5ef19ff40
MUKILTEO\1725608 <Talonlee44@gmail.com>
2026-03-30 10:02:08 -0700

    parrallax works

--- Assets/TalonScripts/MoveBackground.cs
+++ Assets/TalonScripts/MoveBackground.cs
@@ -4,7 +4,29 @@ using System.Collections.Generic;
 
 public class MoveBackground : MonoBehaviour
 {
-    [SerializeField][Range(0f, 1f)] float _lagAmount = 0f;
+    float startPos, length;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    private void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float distance = (cam.transform.position.x * parallaxEffect); // 0 = moves with the camera, 1 = does not move
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(movement > startPos + length) startPos += length;
+        else if(movement < startPos - length) startPos -= length;
+    }
+
+    // This script is used to create a parallax effect for the background. The background will move at a different speed than the camera, creating a sense of depth. The lag amount can be adjusted to increase or decrease the parallax effect. A lag amount of 0 means the background will move at the same speed as the camera, while a lag amount of 1 means the background will not move at all.
+    /*[SerializeField][Range(0f, 1f)] float _lagAmount = 0f;
 
     Vector3 _previousCameraPosition;
     Transform _camera;
@@ -36,6 +58,6 @@ public class MoveBackground : MonoBehaviour
             _parallaxAmount = ParallaxAmount;
             return movement;
         }
-    }
+    }*/
 }
 
