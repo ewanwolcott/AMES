8507f2bf984626abdec59301911251db7c39d5dd
MUKILTEO\1725608 <Talonlee44@gmail.com>
2026-04-20 09:24:03 -0700

    Added floating option to teleporter

--- Assets/TalonScripts/Teleporter.cs
+++ Assets/TalonScripts/Teleporter.cs
@@ -9,10 +9,16 @@ public class Teleporter : MonoBehaviour
     [SerializeField] Image img;
     [SerializeField] ScreenFader _screenFader;
 
+    [SerializeField] bool floating = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            if(floating)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            }
             collision.gameObject.GetComponent<PlayerMovement>().canMove = false;
             collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
             _screenFader.FadeOut(ScreenFader.FadeType.Shutters);
@@ -24,6 +30,10 @@ public class Teleporter : MonoBehaviour
 
         if (collision.CompareTag("Player") && img.material.GetFloat("_FadeAmount") == 1)
         {
+            if (floating)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
+            }
             collision.gameObject.GetComponent<PlayerMovement>().canMove = true;
             collision.transform.position = teleportLocation;
         }
